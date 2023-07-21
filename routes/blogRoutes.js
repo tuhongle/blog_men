20a860e322929f10a0f31e9ce5df9ae72f3a9ea2
LE HONG TU <hongtufiroct@gmail.com>
2023-07-21 23:39:32 +0700

    1 - PUSH FROM PC

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,12 +4,17 @@ import Blog from '../models/blogModel.js'
 const router = express.Router()
 
 router.get('/create', async (req, res) => {
-   await res.render('newBlog', {title: 'Create Blog'})
+   await res.render('newBlog', {title: 'Create Blog', newBlog: true, home: false, about: false})
 })
 
 router.get('/', async (req, res) => {
-    const result = await Blog.find().sort({createdAt: -1})
-    await res.status(200).render('index', {title: 'Blogs', blogs: result})
+    const page = req.query.page || 0;
+    const blogsPerPage = 3;
+
+    const result = await Blog.find().sort({createdAt: -1}).skip(page * blogsPerPage).limit(blogsPerPage)
+    const totalBlogs = await Blog.find()
+    const blogPages = Math.ceil(totalBlogs.length / blogsPerPage)
+    await res.status(200).render('index', {title: 'Blogs', blogs: result, blogPages, home: true, about: false, newBlog: false})
 })
 
 router.post('/', async (req, res) => {
@@ -21,7 +26,7 @@ router.post('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
     const result = await Blog.findById(id)
-    await res.status(200).render('singleBlog', {title: 'Single Blog', blog: result})
+    await res.status(200).render('singleBlog', {title: 'Single Blog', blog: result, home: false, about: false, newBlog: false})
 })
 
 router.delete('/:id', async(req, res) => {
@@ -33,13 +38,16 @@ router.delete('/:id', async(req, res) => {
 router.get('/:id/edit', async(req, res) => {
     const id = req.params.id;
     const result = await Blog.findById(id);
-    await res.status(200).render('editBlog', { title: 'Edit Blog', blog: result});
+    await res.status(200).render('editBlog', { title: 'Edit Blog', blog: result, home: false, about: false, newBlog: false});
 })
 
 router.patch('/:id', async (req, res) => {
     const id = req.params.id;
-    const result = await Blog.findByIdAndUpdate(id, req.body)
-    await res.status(201).render('singleBlog', { title: 'Single Blog', blog: result})
+    console.log(req.body);
+    const result = await Blog.findByIdAndUpdate(id, req.body, {
+        new: true
+    });
+    await res.status(201).json({redirect: `/blogs/${id}`});
 })
 
 export default router;
